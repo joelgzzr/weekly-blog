8bb20ceec99f61067ebc88145196679107e16cc8
Joel <joelgzzjr@hotmail.com>
2019-12-18 09:37:42 -0600

    removing unused libraries

--- react-ssr-node/src/server/lib/renderer.js
+++ react-ssr-node/src/server/lib/renderer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
 import serialize from 'serialize-javascript';
 import { Helmet } from 'react-helmet';
